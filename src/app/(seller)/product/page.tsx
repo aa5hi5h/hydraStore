940a1b792ea4e0eab2f00ad90a8179e2df5d358d
aashish <quakie.exe@gmail.com>
2024-06-23 18:48:17 +0530

    Prodcut Page setup

--- src/app/(seller)/product/page.tsx
+++ src/app/(seller)/product/page.tsx
@@ -1,4 +1,3 @@
-import BuyerStoreDetail from "@/components/BuyerStoreDetail"
 import FeaturedShopsLists from "@/components/FeaturedShopsList"
 import FeaturedProductsItems from "@/components/FeturedProductsItems"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
@@ -8,15 +7,8 @@ import StoreCard from "@/components/StoreCard"
 
 const Page = () => {
 
-
-    const storeList = [
-        {name:"Store Name", description:'Shop description',products:"10",href:"/storepage"},
-        {name:"Store Name", description:'Shop description',products:"10",href:"/storepage"},
-        {name:"Store Name", description:'Shop description',products:"10",href:"/storepage"},
-        {name:"Store Name", description:'Shop description',products:"10",href:"/storepage"},
-    ]
-
         const itemsList = [
+            {name:"Product name", image:'ItemsImage', description:'Items description',price:"Items Price",href:"/productpage"},
             {name:"Product name", image:'ItemsImage', description:'Items description',price:"Items Price",href:"/productpage"},
             {name:"Product name", image:'ItemsImage', description:'Items description',price:"Items Price",href:"/productpage"},
             {name:"Product name", image:'ItemsImage', description:'Items description',price:"Items Price",href:"/productpage"},
@@ -24,53 +16,52 @@ const Page = () => {
         ]
     return (
         <div className="flex flex-col p-6">
-            <h1 className="text-2xl font-bold tracking-tight">Trending stores this week</h1>
+            <h1 className="text-2xl font-bold tracking-tight">Trending Products this week</h1>
             <div className="grid grid-cols-2 md:grid-cols-5 gap-6 mt-8 px-4">
                     {
-                        storeList.map((product,index) => (
+                        itemsList.map((product,index) => (
                             <div key={index} className="">
-                            <FeaturedShopsLists key={product.href} name={product.name} description={product.description} product={product.products} href={product.href} />
+                            <FeaturedProductsItems key={product.href} name={product.name} description={product.description} image={product.image} price={product.price} href={product.href} />
                         </div>)
 
                         )
                     }
                     </div>
-            <h1 className="text-2xl font-bold tracking-tight pt-[2.5rem] mt-4">Store currently have sales</h1>
+            <h1 className="text-2xl font-bold tracking-tight pt-[2.5rem] mt-4">Products currently have sales</h1>
             <div className="grid grid-cols-2 md:grid-cols-5 gap-6 mt-8 px-4">
                     {
-                        storeList.map((product,index) => (
+                        itemsList.map((product,index) => (
                             <div key={index} className=" col-span-1">
-                            <FeaturedShopsLists key={product.href} name={product.name} description={product.description} product={product.products} href={product.href} />
+                            <FeaturedProductsItems key={product.href} name={product.name} description={product.description} image={product.image} price={product.price} href={product.href} />
                         </div>)
 
                         )
                     }
                     </div>
-                <h1 className="text-2xl font-bold tracking-tight pt-[2.5rem] mt-4">Featured stores</h1>
-                <div className="flex flex-col gap-6 mt-8">
+                <h1 className="text-2xl font-bold tracking-tight pt-[2.5rem] mt-4">Featured Prodcuts</h1>
+                <div className="grid grid-cols-2 md:grid-cols-5 gap-6 mt-8 px-4">
                     {
-                        storeList.map((product,index) => (
-                        <StoreCard key={index} />
+                        itemsList.map((product,index) => (
+                         <FeaturedProductsItems key={index} name={product.name} description={product.description} image={product.image} href={product.href} price={product.price} />
                         ))
                     }
                 </div>
                 <h1 className="text-2xl font-bold tracking-tight pt-[2.5rem] mt-4">Clothing Products</h1>
-                <div className="flex flex-col gap-6 mt-8">
+                <div className="grid grid-cols-2 md:grid-cols-5 gap-6 mt-8 px-4">
                     {
-                        storeList.map((product,index) => (
-                        <StoreCard key={index} />
+                        itemsList.map((product,index) => (
+                        <FeaturedProductsItems key={index} name={product.name} description={product.description} image={product.image} href={product.href} price={product.price}  />
                         ))
                     }
                 </div>
                 <h1 className="text-2xl font-bold tracking-tight pt-[2.5rem] mt-4">Accessories</h1>
-                <div className="flex flex-col gap-6 mt-8">
+                <div className="grid grid-cols-2 md:grid-cols-5 gap-6 mt-8 px-4">
                     {
-                        storeList.map((product,index) => (
-                        <StoreCard key={index} />
+                        itemsList.map((product,index) => (
+                        <FeaturedProductsItems key={index} name={product.name} description={product.description} image={product.image} href={product.href} price={product.price} />
                         ))
                     }
                 </div>
-
         </div>
     )
 }
