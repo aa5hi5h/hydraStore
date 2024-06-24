63f3e800c2a413e9077217e0ef1d1b5f94391e04
aashish <quakie.exe@gmail.com>
2024-06-24 17:14:35 +0530

    addedd the animated product carousel in the store

--- src/components/StoreCard.tsx
+++ src/components/StoreCard.tsx
@@ -1,9 +1,10 @@
+"use client"
 import { ArrowUpRight, Image } from "lucide-react"
 import ProductsCaraousel from "./ProductsCaraousel"
-
-
+import {animate, motion, useMotionValue} from "framer-motion"
+import useMeasure from "react-use-measure"
+import { useEffect } from "react"
 const StoreCard = () => {
-
 
     const itemsList = [
         {name:"Product name", image:'ItemsImage', description:'Items description',price:"Items Price",href:"/productpage"},
@@ -11,6 +12,28 @@ const StoreCard = () => {
         {name:"Product name", image:'ItemsImage', description:'Items description',price:"Items Price",href:"/productpage"},
         {name:"Product name", image:'ItemsImage', description:'Items description',price:"Items Price",href:"/productpage"},
     ]
+
+    let[ref,{width}] = useMeasure()
+
+        const xStyles = useMotionValue(0)
+
+        useEffect(() => {
+            let controls
+            let finsihPosition = -width -170
+
+            console.log(width,finsihPosition)
+
+            controls = animate(xStyles,[0,finsihPosition],{
+                ease:"linear",
+                duration: 15,
+                repeat: Infinity,
+                repeatType: "loop",
+                repeatDelay: 0,
+        })
+
+            return controls.stop
+
+        },[xStyles,width])
 
 
     return (
@@ -21,11 +44,17 @@ const StoreCard = () => {
             <div className="flex flex-col w-[65vh] md:w-[150vh] rounded-b-lg mx-auto border-[1px] border-blue-200 p-4">
                 <h1 className="text-2xl font-bold tracking-tight ">Products</h1>
                 <div className=" flex gap-[4rem] px-2 py-8 ">
+                <div className="overflow-hidden">
+                <motion.div className="grid grid-cols-2 md:grid-cols-5 gap-6 mt-8 px-4" ref={ref} style={{x:xStyles}} >
+                    <div className="flex gap-8">
                     {
-                        itemsList.map((items,index) => (
-                            <ProductsCaraousel key={index} name={items.name} description={items.description} image={items.image} href={items.href} price={items.price}  />
+                        [...itemsList,...itemsList].map((product,index) => (
+                        <ProductsCaraousel key={index} name={product.name} description={product.description} image={product.image} href={product.href} price={product.price}  />
                         ))
                     }
+                    </div>
+                </motion.div>
+                </div>
                 </div>
             </div>
         </div>
